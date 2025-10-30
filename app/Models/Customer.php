4eda71b26b368ce1884345d1c220cac1a900cf30
Setup Connect 3 <setup_connect3@dmc.edu.ph>
2025-10-30 11:57:02 +0000

    Merged PR 4: 34 [Customer Model and Migration] [Fix issues] - BaguioCode
    Fix issues
    Required data should not be nullable.
    Remove all columns for the type of assistance sought, this system is implicitly expected to only handle SETUP funding.
    Separate the name column into three: first_name, middle_name, and last_name.
    Add a column for suffix.
    Don't shorten the column names. Keep them similar to the CUSTOMER'S PROFILE FORM. E.g. business_of_the_firm, name_of_head_of_agency_firm, website_email_address. But keep those that have already been lengthened for readability such as, accomplished_date, handled_date, and noted_date.
    Change customer model to customer application model along with related migrations to better represent what it is.

---
+++ app/Models/Customer.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'customer_applications';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'middle_name',
+        'last_name',
+        'suffix',
+        'designation_position',
+        'residential_address',
+        'name_of_agency_firm',
+        'business_of_the_firm',
+        'product_line',
+        'type_of_organization',
+        'date_established',
+        'name_of_head_of_agency_firm',
+        'business_address',
+        'contact_nos',
+        'website_email_address',
+        'accomplished_by',
+        'accomplished_date',
+        'remarks_action_taken',
+        'handled_by',
+        'handled_date',
+        'remarks_by_cpstd',
+        'noted_by',
+        'noted_date',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'date_established' => 'date',
+            'accomplished_date' => 'date',
+            'handled_date' => 'date',
+            'noted_date' => 'date',
+        ];
+    }
+}
---
+++ database/migrations/2025_10_30_055727_create_customers_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customer_applications', function (Blueprint $table) {
+            $table->id();
+            // Basic Information
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->string('suffix')->nullable();
+            $table->string('designation_position');
+            $table->text('residential_address');
+            $table->string('name_of_agency_firm');
+            $table->string('business_of_the_firm');
+            $table->string('product_line');
+            $table->string('type_of_organization');
+            $table->date('date_established');
+            $table->string('name_of_head_of_agency_firm');
+            $table->text('business_address');
+            $table->string('contact_nos');
+            $table->string('website_email_address');
+
+            // Processing Information
+            $table->string('accomplished_by')->nullable();
+            $table->date('accomplished_date')->nullable();
+            $table->text('remarks_action_taken')->nullable();
+            $table->string('handled_by')->nullable();
+            $table->date('handled_date')->nullable();
+            $table->text('remarks_by_cpstd')->nullable();
+            $table->string('noted_by')->nullable();
+            $table->date('noted_date')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customer_applications');
+    }
+};
